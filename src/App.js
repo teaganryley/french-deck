6fac094a22f665bf4269aab510af0f184fb81d20
tea <teaganschultz@gmail.com>
2020-09-30 13:16:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+  //styles go here
+  paper: {
+    marginTop: theme.spacing(15),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+}));
 
 function App() {
+  const [deckNum, setDeckNum] = useState(1);
+  const classes = useStyles();
+
+  const handleSubmit = (event) => {
+    window.alert('submitted!');
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3} className={classes.paper}>
+        Placeholder!
+      </Paper>
+    </Container>
   );
 }
 
